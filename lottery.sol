aa06657762ae76f0393209939776ff7d976a5085
Fábio A. M. Pereira <famp@operamail.com>
2021-07-30 19:16:09 -0300

    Mais algumas modificações

--- lottery.sol
+++ lottery.sol
@@ -7,8 +7,10 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract Lottery {
     
-    uint64 public acumulatedPrize = 0;      // Valor acumulado do premio do sorteio
-    address public winnerAddress;           // Esse é o endereço do vencedor do sorteio
+    address lotteryAddress;
+    uint256 acumulatedPrize = 0;        // Valor acumulado do premio do sorteio
+    address payable winnerAddress;           // Esse é o endereço do vencedor do sorteio
+    uint64 public ticketPrice = 1;
     Participant[] public participants;      // Essa é a lista de participantes do sorteio
     
     
@@ -18,31 +20,33 @@ contract Lottery {
         bool isPaid;            // Variável de controle para saber se o ticket foi pago realmente
     }
     
-    // Mapeamento para saber quanto ether uusário tem
-    mapping(address => uint64) equity_ether;
     
     // Aqui inicializamos e limpamos a lista de participantes para cada novo sorteio
-    function initParticipantsList() public {
+    function initParticipantsList() private {
         while(participants.length > 0) {
             participants.pop();
         }
     }
     
     // Aqui registramos um participante no sorteio
-    function registerParticipant(address participantAddr) public {
-        participants.push(Participant({ ticketNumber: participantAddr, isPaid: true }));
+    function registerParticipant(address participantAddress) public {
+        participants.push(Participant({ ticketNumber: participantAddress, isPaid: true }));
     }
     
     // Aqui selecionamos o vencedor do sorteio
     function selectWinner() public {
-        
+        //uint participantsListLen = participants.length;
     }
     
     // Aqui o contrato envia o premio ao vencedor
-    function sendWinnerPrize() public {
+    function sendPrize() public {
+        // Envia o valor acumulado ao vencedor
+        winnerAddress.transfer(acumulatedPrize);
         // Agora que o premio foi pago, retorna o valor acumulado para 0
         acumulatedPrize = 0;
     }
     
-
+    function getAcumulatedPrize() public view returns (uint256) {
+        return acumulatedPrize;
+    }
 }
